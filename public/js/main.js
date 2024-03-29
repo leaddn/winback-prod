050b2562fc2ea498b578f4bcbca7c4cb43ad849b
leaddn <leadieudonat@gmail.com>
2024-03-29 11:23:29 +0100

    protocol

--- public/js/main.js
+++ public/js/main.js
@@ -85,3 +85,21 @@ $("#cookie_consent_save").click(function () {
       })
   })
 })()
+
+$(".count").each(function () {
+  $(this)
+    .prop("Counter", 0)
+    .animate(
+      {
+        Counter: $(this).text(),
+      },
+      {
+        duration: 1000,
+        easing: "swing",
+        step: function (now) {
+          now = Number(Math.ceil(now)).toLocaleString('en');
+                                $(this).text(now);
+        },
+      }
+    );
+});
