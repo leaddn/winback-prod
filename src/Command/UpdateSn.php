163178b22c04c7f734c80a4e220e2d3363880b66
leaddn <leadieudonat@gmail.com>
2024-04-03 10:02:46 +0200

    update sn

--- src/Command/UpdateSn.php
+++ src/Command/UpdateSn.php
@@ -26,14 +26,19 @@ class UpdateSn extends Command
 
         $helper = $this->getHelper('question');
         #fileQuestion
-        $fileQuestion = new Question('Please enter filename: ', 'PRODUCT_SELLING_2024');
+        //$fileQuestion = new Question('Please enter filename: ', 'PRODUCT_SELLING_2024');
+        $fileQuestion = new ChoiceQuestion('Please enter filename: ', 
+        ['2024 PRODUCT SELLING 2024_EDITED', 'PRODUCT_SELLING_2024'],
+        0);
         $filename = $helper->ask($input, $output, $fileQuestion);
         $output->writeln('You have just selected: '.$filename);
         
         #monthQuestion
         $monthQuestion = new ChoiceQuestion(
             'Please select month ',
-            ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+            //['JAN'=>1, 'FEB'=>2, 'MAR'=>3, 'APR'=>4, 'MAY'=>5, 'JUN'=>6, 'JUL'=>7, 'AUG'=>8, 'SEP'=>9, 'OCT'=>10, 'NOV'=>11, 'DEC'=>12],
+            //[1=>'JAN', 2=>'FEB', 3=>'MAR', 4=>'APR', 5=>'MAY', 6=>'JUN', 7=>'JUL', 8=>'AUG', 9=>'SEP', 10=>'OCT', 11=>'NOV', 12=>'DEC'],
+            [1=>'1', 2=>'2', 3=>'3', 4=>'4', 5=>'5', 6=>'6', 7=>'7', 8=>'8', 9=>'9', 10=>'10', 11=>'11', 12=>'12'],
             0
         );
         $monthQuestion->setErrorMessage('Month %s is invalid.');
